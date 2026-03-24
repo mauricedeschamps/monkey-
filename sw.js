b9e1bd42eefa930471d38b378ca8eba7202fce22
mauricedeschamps <mementomorikatsu@gmail.com>
2026-03-24 13:26:13 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
 const CACHE_NAME = 'punch-kun-v3';
 const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
+ '/monkey-/',
+ '/monkey-/index.html',
+  '/monkey-/manifest.json',
+  '/monkey-/icon-192.png',
+  '/monkey-/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
